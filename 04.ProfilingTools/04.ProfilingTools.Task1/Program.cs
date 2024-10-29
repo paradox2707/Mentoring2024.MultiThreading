1332173aabb79797d088e316f0c8a8660a158397
Andrii Slyzhuk <Andrii_Slyzhuk@epam.com>
2024-10-29 23:42:39 +0200

    revert task 1 to initial state

--- 04.ProfilingTools/04.ProfilingTools.Task1/Program.cs
+++ 04.ProfilingTools/04.ProfilingTools.Task1/Program.cs
@@ -8,8 +8,7 @@ namespace _04.ProfilingTools.Task1
         {
             Console.WriteLine("Enter password for hashing");
             var passwordText = Console.ReadLine() ?? string.Empty;
-            //var passHash = GeneratePasswordHashUsingSalt(passwordText, GenerateSalt());
-            var passHash = GeneratePasswordHashUsingSalt_Optimized(passwordText, GenerateSalt());
+            var passHash = GeneratePasswordHashUsingSalt(passwordText, GenerateSalt());
             Console.WriteLine($"Password hash is {passHash}");
             Console.ReadKey();
         }
@@ -28,27 +27,6 @@ namespace _04.ProfilingTools.Task1
             var passwordHash = Convert.ToBase64String(hashBytes);
 
             return passwordHash;
-
-        }
-
-        public static string GeneratePasswordHashUsingSalt_Optimized(string passwordText, byte[] salt)
-        {
-
-            const int iterate = 10000; // Number of iterations
-            const int hashSize = 20; // Size of the hash in bytes
-            const int totalSize = 36; // Total size of the resulting byte array (16 bytes salt + 20 bytes hash)
-
-            using (var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate))
-            {
-                byte[] hash = pbkdf2.GetBytes(hashSize);
-                byte[] hashBytes = new byte[totalSize];
-
-                // Copy salt and hash into the result array
-                Buffer.BlockCopy(salt, 0, hashBytes, 0, salt.Length); // Copy salt
-                Buffer.BlockCopy(hash, 0, hashBytes, salt.Length, hash.Length); // Copy hash
-
-                return Convert.ToBase64String(hashBytes);
-            }
 
         }
 
