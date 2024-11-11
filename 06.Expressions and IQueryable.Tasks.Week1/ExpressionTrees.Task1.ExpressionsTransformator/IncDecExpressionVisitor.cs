c7889ac0726a268695f6905952d24aa1dda2b016
Andrii Slyzhuk <Andrii_Slyzhuk@epam.com>
2024-11-11 23:02:41 +0200

    task 1

--- 06.Expressions and IQueryable.Tasks.Week1/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
+++ 06.Expressions and IQueryable.Tasks.Week1/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -1,9 +1,59 @@
-﻿using System.Linq.Expressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace ExpressionTrees.Task1.ExpressionsTransformer
 {
     public class IncDecExpressionVisitor : ExpressionVisitor
     {
-        // todo: feel free to add your code here
+        private readonly Dictionary<string, object> _parameterReplacements;
+
+        public IncDecExpressionVisitor(Dictionary<string, object> parameterReplacements)
+        {
+            _parameterReplacements = parameterReplacements;
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            // First, visit the left and right nodes to apply parameter replacements
+            var left = Visit(node.Left);
+            var right = Visit(node.Right);
+
+            // Handle increment: <variable> + 1
+            if (node.NodeType == ExpressionType.Add &&
+                right is ConstantExpression rightConst &&
+                rightConst.Value.Equals(1))
+            {
+                return Expression.Increment(left);
+            }
+
+            // Handle decrement: <variable> - 1
+            if (node.NodeType == ExpressionType.Subtract &&
+                right is ConstantExpression rightConstSub &&
+                rightConstSub.Value.Equals(1))
+            {
+                return Expression.Decrement(left);
+            }
+
+            // Return the rebuilt binary expression with replaced nodes
+            return Expression.MakeBinary(node.NodeType, left, right);
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            // Replace parameter with constant if a replacement is provided
+            if (_parameterReplacements.TryGetValue(node.Name, out var replacementValue))
+            {
+                return Expression.Constant(replacementValue, node.Type);
+            }
+
+            return node; // Return the original node if no replacement is found
+        }
+
+        public Expression Transform(Expression expression)
+        {
+            // Visit the expression tree and replace parameters with constants
+            return Visit(expression);
+        }
     }
 }
--- 06.Expressions and IQueryable.Tasks.Week1/ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
+++ 06.Expressions and IQueryable.Tasks.Week1/ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
@@ -7,6 +7,8 @@
  * The results could be printed in console or checked via Debugger using any Visualizer.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace ExpressionTrees.Task1.ExpressionsTransformer
 {
@@ -17,7 +19,35 @@ namespace ExpressionTrees.Task1.ExpressionsTransformer
             Console.WriteLine("Expression Visitor for increment/decrement.");
             Console.WriteLine();
 
-            // todo: feel free to add your code here
+            // Example expression: x => x + 1
+            Expression<Func<int, int>> expression = x => x + 1;
+
+            // Create a dictionary for parameter replacement
+            Console.WriteLine("Enter value(only int) for increment/decrement.");
+            var inputValue = Int32.Parse(Console.ReadLine());
+            var replacements = new Dictionary<string, object>
+            {
+                { "x", inputValue } 
+            };
+
+            // Create and apply the visitor
+            var visitor = new IncDecExpressionVisitor(replacements);
+            var transformedBody = visitor.Transform(expression.Body);
+
+            // Create a new lambda expression without parameters
+            var transformedExpression = Expression.Lambda<Func<int>>(transformedBody);
+
+            // Display the transformed expression
+            Console.WriteLine("Transformed Expression: " + transformedExpression);
+
+            // Compile the transformed expression into a delegate
+            var compiledExpression = transformedExpression.Compile();
+
+            // Execute the compiled expression and get the result
+            int result = compiledExpression(); // No parameter needed as x is replaced
+
+            // Output the result
+            Console.WriteLine($"Result of the expression: {result}"); // Should output 6
 
             Console.ReadLine();
         }
