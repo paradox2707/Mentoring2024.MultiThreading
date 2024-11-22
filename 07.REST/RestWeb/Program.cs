456f4f5b5eadb81c72176afb861cf71a8185672f
Andrii Slyzhuk <Andrii_Slyzhuk@epam.com>
2024-11-23 01:24:50 +0200

    deployment to local IIS

--- 07.REST/RestWeb/Program.cs
+++ 07.REST/RestWeb/Program.cs
@@ -34,11 +34,11 @@ namespace RestWeb
             }
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
             app.UseAuthorization();
