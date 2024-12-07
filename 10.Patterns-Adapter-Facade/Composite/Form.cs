fc0a0a33bd5a3e3378f6b3fbf7e6115d76f78d45
Andrii Slyzhuk <Andrii_Slyzhuk@epam.com>
2024-12-07 22:32:56 +0200

    added Composite

---
+++ 10.Patterns-Adapter-Facade/Composite/Form.cs
@@ -0,0 +1,30 @@
+﻿namespace Composite
+{
+    public class Form : IXmlElement
+    {
+        private readonly string _name;
+        private readonly List<IXmlElement> _elements;
+
+        public Form(string name)
+        {
+            _name = name;
+            _elements = new List<IXmlElement>();
+        }
+
+        public void AddComponent(IXmlElement element)
+        {
+            _elements.Add(element);
+        }
+
+        public string ConvertToString()
+        {
+            var xml = $"<form name='{_name}'>\n";
+            foreach (var element in _elements)
+            {
+                xml += $"  {element.ConvertToString()}\n";
+            }
+            xml += "</form>";
+            return xml;
+        }
+    }
+}
