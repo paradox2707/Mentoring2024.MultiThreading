60cc6ed8b0c052051260b5ea3451238413efa04b
Andrii Slyzhuk <Andrii_Slyzhuk@epam.com>
2025-02-09 21:19:07 +0200

    added using for task 1

--- 04.ProfilingTools/04.ProfilingTools.Task1/Program.cs
+++ 04.ProfilingTools/04.ProfilingTools.Task1/Program.cs
@@ -16,19 +16,21 @@ namespace _04.ProfilingTools.Task1
         public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
         {
             var iterate = 10000;
-            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
-            byte[] hash = pbkdf2.GetBytes(20);
+            using (var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate))
+            {
+                byte[] hash = pbkdf2.GetBytes(20);
 
-            byte[] hashBytes = new byte[36];
+                byte[] hashBytes = new byte[36];
 
-            Parallel.Invoke(
-                () => Array.Copy(salt, 0, hashBytes, 0, 16),
-                () => Array.Copy(hash, 0, hashBytes, 16, 20)
-            );
+                Parallel.Invoke(
+                    () => Array.Copy(salt, 0, hashBytes, 0, 16),
+                    () => Array.Copy(hash, 0, hashBytes, 16, 20)
+                );
 
-            var passwordHash = Convert.ToBase64String(hashBytes);
+                var passwordHash = Convert.ToBase64String(hashBytes);
 
-            return passwordHash;
+                return passwordHash;
+            }
         }
 
         public static byte[] GenerateSalt(int size = 16) // Default size is 16 bytes
